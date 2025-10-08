2f27820431cb7aa5b149543528b3bdf9a94e2f27
Razvan Dumitrescu <razvanfr215@gmail.com>
2025-10-08 21:38:31 +0300

    feat: render cars on homepage

---
+++ app.js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded", displayCars);
+const URL = "https://68e3ee488e116898997a79e1.mockapi.io/cars";
+function displayCars() {
+  fetch(URL)
+    .then((response) => {
+      if (response.ok === false) {
+        throw new Error("Network error!");
+      } else return response.json();
+    })
+    .then(
+      (cars) =>
+        (document.querySelector(".cars-container").innerHTML = cars
+          .map(
+            (car) =>
+              `<div class="car-card">
+        <img
+          src=${car.imageURL}
+          alt="Car image"
+        />
+        <div class="car-info">
+          <h3>${car.marca} ${car.model}</h3>
+          <div class="price">${car.pret} &#8364</div>
+          <h4>${car.details}</h4>
+          <h4>📅 ${car.anFabricatie}</h4>
+          <h4>🛞 ${car.km} KM</h4>
+          <div class="buttons">
+            <button class="details-btn">Details</button>
+            <button class="cart-btn">Add to cart</button>
+          </div>
+        </div>
+      </div>`
+          )
+          .join(""))
+    );
+}
