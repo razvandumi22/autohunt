3dcd2504172fe948b2adc5749284b9acb682303c
Razvan Dumitrescu <razvanfr215@gmail.com>
2025-10-28 21:09:04 +0200

    Update Admin page with edit/delete buttons and functionality

--- admin.js
+++ admin.js
@@ -3,7 +3,17 @@ window.addEventListener("DOMContentLoaded", renderTable);
 const URL = "https://68e3ee488e116898997a79e1.mockapi.io/cars";
 
 const tableBody = document.querySelector("#cars-table tbody");
-const addBtn = document.querySelector("#add-btn");
+const addOrEditBtn = document.querySelector("#add-or-edit-btn");
+let isEditMode = false;
+let carId;
+
+const marcaInput = document.getElementById("marca");
+const modelInput = document.getElementById("model");
+const priceInput = document.getElementById("price");
+const imageURLInput = document.getElementById("imageURL");
+const descriptionInput = document.getElementById("description");
+const kmInput = document.getElementById("km");
+const anFabricatieInput = document.getElementById("anFabricatie");
 
 function renderTable() {
   fetch(URL)
@@ -44,31 +54,89 @@ function renderTable() {
     });
 }
 
-addBtn.addEventListener("click", addNewCar);
+addOrEditBtn.addEventListener("click", addOrEditCar);
 
-function addNewCar(e) {
+function addOrEditCar(e) {
   e.preventDefault();
-  const marca = document.getElementById("marca").value;
-  const model = document.getElementById("model").value;
-  const price = document.getElementById("price").value;
-  const imageURL = document.getElementById("imageURL").value;
-  const description = document.getElementById("description").value;
-  const km = document.getElementById("km").value;
-  const anFabricatie = document.getElementById("anFabricatie").value;
+  const marca = marcaInput.value;
+  const model = modelInput.value;
+  const price = priceInput.value;
+  const imageURL = imageURLInput.value;
+  const description = descriptionInput.value;
+  const km = kmInput.value;
+  const anFabricatie = anFabricatieInput.value;
 
   const newCar = {
     marca: marca,
     model: model,
-    price: price,
+    pret: price,
     imageURL: imageURL,
     details: description,
     km: km,
     anFabricatie: anFabricatie,
   };
+  const method = isEditMode ? "PUT" : "POST";
+  const newUrl = isEditMode ? `${URL}/${carId}` : URL;
 
-  fetch(URL, {
-    method: "POST",
+  fetch(newUrl, {
+    method: method,
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(newCar),
-  }).then((response) => renderTable());
+  }).then((response) => {
+    renderTable();
+    resetForm();
+  });
+
+  function resetForm() {
+    marcaInput.value = "";
+    modelInput.value = "";
+    priceInput.value = "";
+    imageURLInput.value = "";
+    descriptionInput.value = "";
+    kmInput.value = "";
+    anFabricatieInput.value = "";
+
+    if (isEditMode) {
+      isEditMode = false;
+      addOrEditBtn.innerHTML = "Add car";
+    }
+  }
 }
+
+tableBody.addEventListener("click", handleActions);
+
+function handleActions(e) {
+  const clickedElement = e.target;
+  if (clickedElement.parentElement.classList.contains("edit")) {
+    carId = getTableRow(clickedElement).dataset.id;
+    fetch(`${URL}/${carId}`)
+      .then((response) => response.json())
+      .then((car) => {
+        console.log(car);
+        marcaInput.value = car.marca;
+        modelInput.value = car.model;
+        priceInput.value = car.pret;
+        imageURLInput.value = car.imageURL;
+        descriptionInput.value = car.details;
+        kmInput.value = car.km;
+        anFabricatieInput.value = car.anFabricatie;
+      });
+    isEditMode = true;
+    console.log(addOrEditBtn, isEditMode);
+    addOrEditBtn.innerHTML = "Save";
+  } else if (clickedElement.parentElement.classList.contains("delete")) {
+    productId = getTableRow(clickedElement).dataset.id;
+    fetch(`${URL}/${productId}`, {
+      method: "DELETE",
+    }).then((response) => renderTable());
+  }
+}
+
+function getTableRow(editIcon) {
+  return editIcon.parentElement.parentElement.parentElement.parentElement;
+}
+// buton de add product, se transform in add or edit si schimbam id-ul
+// cream o variabila de mod edit in care stocam true daca editam sau false daca adaugam(default value)
+// in momentul in care punem in input datele dintr-un produs care urmeaza sa fie editat, atunci variabila de edit mode se duce la true si i se schimba continutul din add product in save
+// metodele si numele de variabile pentru addNewProduct se transorma in ceva care se duca cu gandul ca si editam, exemplu: addOrEditBtn
+// la metoda care facea post trebuie sa adaugam o variabila method care va fi fie POST fie PUT in functie de valoarea lui isEditMode folosind ternary operator.
